82f6ee4436ce47069982b941bb105358c124d2d0
Hyunmin Lee <hyunmin@toss.im>
2021-03-28 18:52:27 +0900

    ch23. Fizzbuzz

---
+++ ch23/fizzbuzz.hs
@@ -0,0 +1,9 @@
+fizzBuzz :: Integer -> String
+fizzBuzz n
+  | n `mod` 15 == 0 = "FizzBuzz"
+  | n `mod` 5 == 0 = "Buzz"
+  | n `mod` 3 == 0 = "Fizz"
+  | otherwise = show n
+
+main = 
+    mapM_ (putStr . fizzBuzz) [1..100]
