03361d71b5419428dc3d0fc2f0ef954950a13fee
qpzm <lhm1442@gmail.com>
2021-06-07 03:00:32 +0900

    Ch11. Refactor Capitalize: use Monoid

--- ch11/Capitalize.hs
+++ ch11/Capitalize.hs
@@ -5,10 +5,27 @@ capitalizeWord :: String -> String
 capitalizeWord [] = []
 capitalizeWord (head : tail) = toUpper head : tail
 
+-- Delete trailing whitespace through trimRight -> 이걸 어떻게 뺄 수 있을까?
 capitalizeParagraph :: String -> String
-capitalizeParagraph str = trimRight . foldr ((\x y -> x ++ ". " ++ y) . capitalizeWord . trimLeft) "" $ split '.' str
+-- Solution 1.
+--capitalizeParagraph str = trimRight . foldr ((\x y -> x ++ ". " ++ y) . capitalizeWord . trimLeft) "" $ split '.' str
+
+-- Solution 2.
+capitalizeParagraph = trimRight . join ". " . map (capitalizeWord . trimLeft) . split '.'
+
+join :: Monoid a => a -> [a] -> a
+join d = foldr (\x y -> x <> d <> y) mempty
+
+-- 위처럼 추상화하기 전
+--join :: String -> [String] -> String
+--join d [] = []
+--join d (x:xs) = x ++ foldr(\x y -> x ++ d ++ y) "" xs
 
 -- Reference : https://stackoverflow.com/a/24792141/9379031
+-- Calculation Step
+-- split "blah. woot ha"
+-- "blah" : drop 1 ". woot ha"
+-- "blah" : " woot ha"
 split :: Eq a => a -> [a] -> [[a]]
 split d [] = []
 split d s = x : split d (drop 1 y) where (x,y) = span (/= d) s
@@ -27,5 +44,5 @@ trim = trimRight. trimLeft
 --lstrip (head : tail) = head : tail
 
 main = do
-    let s = "blah. woot ha."
-    print $ capitalizeParagraph s  == "Blah. Woot ha."
+    let s = "blah. woot ha. hihi."
+    print $ capitalizeParagraph s == "Blah. Woot ha. Hihi."
